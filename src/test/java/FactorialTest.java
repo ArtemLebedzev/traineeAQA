cf9b709bdd567c822037c750d34a1e865408f3d4
ArtemLebedzev <artskillsdeveloping@gmail.com>
2024-08-29 20:55:27 +0300

    testNGCommit1

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,42 +1,56 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+/*import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import javax.management.ConstructorParameters;
-import java.time.Duration;
+import org.junit.jupiter.api.Test;*/
+import org.testng.annotations.*;
 
 public class FactorialTest {
     Factorial factorial = new Factorial();
-
-    @DisplayName("Factorial of negative number")
-    @Test
-    public void testFactorialOfNegativeNumber(){
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> factorial.countFactorial(-1));
+    @DataProvider(name = "positiveData")
+    Object[][] positiveNumberDataProvider(){
+        return new Object[][]{
+                {1,1},
+                {2,2},
+                {3,6},
+                {4,24},
+                {5,120}
+        };
     }
 
-    @DisplayName("Factorial of zero")
-    @Test
+    @DataProvider(name = "negativeData")
+    Object[][] negativeNumberDataProvider(){
+        return new Object[][]{
+                {-1},
+                {-10},
+                {-100}
+        };
+    }
+
+    @Test(dataProvider = "negativeData", groups = "negativeTests", description ="Factorial of negative number",
+            dependsOnGroups = "positiveTests")
+    public void testFactorialOfNegativeNumber(int actual){
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> factorial.countFactorial(actual));
+    }
+
+    @Test(groups = "positiveTests", description = "Factorial of zero")
     public void testFactorialOfZero(){
         Assertions.assertEquals(1,factorial.countFactorial(0));
     }
 
-    @DisplayName("Factorial of positive number")
-    @ParameterizedTest
-    @ValueSource(ints={5})
-    public void testFactorialOfPositiveNumber(int n){
-        Assertions.assertEquals(120, factorial.countFactorial(n));
+    @Test(dataProvider = "positiveData", groups = "positiveTests", description = "Factorial of positive number")
+    public void testFactorialOfPositiveNumber(int actual, int expected){
+        Assertions.assertEquals(expected, factorial.countFactorial(actual));
     }
 
-    @DisplayName("Factorial of 12 (max int)")
-    @Test
+    @Test(groups = "boundaryTests", description = "Factorial of 12 (max int)",
+            dependsOnGroups = {"positiveTests", "negativeTests"})
     void testFactorialOfMaxNumber() {
         Assertions.assertEquals(479001600, factorial.countFactorial(12));
     }
 
-    @DisplayName("Factorial of number > 12")
-    @Test
+    @Test(groups = "boundaryTests", description = "Factorial of 12 (max int)",
+            dependsOnMethods = "testFactorialOfMaxNumber")
     public void testFactorialOfTooLargeNumber(){
         Assertions.assertThrows(ArithmeticException.class, ()-> factorial.countFactorial(13));
     }
